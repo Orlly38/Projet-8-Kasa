a7eecb47e1bb1cb969fa182ee2dde49c5b68f0fc
Aurélie <dorche.aurelie@gmail.com>
2023-10-27 19:11:41 +0200

    #11

--- frontend/kasa/src/Pages/Error.jsx
+++ frontend/kasa/src/Pages/Error.jsx
@@ -6,11 +6,13 @@ export default function Error() {
     return (
       <div className="error">
         <Header />
-        <h1 className="error_404"> 404 </h1>
-        <div className="error_oups">
-        Oups! La page que vous demandez n'existe pas.
+        <div className="error_container">
+          <h1 className="error_404"> 404 </h1>
+          <div className="error_oups">
+            Oups! La page que vous demandez n'existe pas.
+          </div>
         </div>
-        <a href="error_return" className="error_return">Retourner à la page d'accueil</a>
+          <a href="/accueil" className="error_return">Retourner à la page d'accueil</a>
         <Footer />
       </div>
     );
